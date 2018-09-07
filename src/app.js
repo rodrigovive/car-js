e9aa6e5acfe1274de468fceb388b8ce231ddc6aa
Rodrigo <cherry199662@gmail.com>
2018-09-07 13:03:35 -0500

    Added Image, TitleBar Class

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import {Button as MainButton} from './ui/button.js';
-import $ from 'jquery'
-let b = new MainButton('Click me')
+import { Image } from './ui/image.js';
+import $ from 'jquery';
+import {TitleBar} from './ui/title-bar.js';
 
-b.appentToElement($('body'))
+// let b = new MainButton('Click me');
+//
+// b.appentToElement($('body'));
+//
+// let i = new Image('images/drone.jpeg');
+//
+// i.appentToElement($('body'));
+//
+
+let tb = new TitleBar('Aplicacion')
+
+tb.addLink('Home','/')
+tb.addLink('Drones','/')
+tb.addLink('Cars','/')
+tb.addLink('Map','/')
+
+tb.appentToElement($('body'))
